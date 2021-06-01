0945118c3688e07a974a6d03ff7ceb11f6270277
Laith Alwani <laithalwani@gmail.com>
2021-06-01 01:06:14 -0400

    finished contact form

--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -1,20 +1,111 @@
+import React, { useEffect, useState } from "react";
 import { makeStyles } from "@material-ui/core";
-import React from "react";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
 
 const useStyles = makeStyles(() => ({
   root: {
-    // padding: "2rem",
-    height:"100vh"
     
+  },
+  form:{
+    width:"50%",
+    heigh:"100%",
+    margin:" 4rem auto",
+  },
+  field: {
+    marginTop: 20,
+    marginBottom: 20,
   },
 }));
 
 function Contact() {
   const classes = useStyles();
-  return (
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [title, setTitle] = useState("");
+  const [message, setMessage] = useState("");
+  const [nameError, setNameError] = useState(false);
+  const [emailError, setEmailError] = useState(false);
+  const [titleError, setTitleError] = useState(false);
+  const [messageError, setMessageError] = useState(false);
+
+  const handleSubmit = e =>{
+    e.preventDefault();
+    setNameError(false);
+    setEmailError(false);
+    setTitleError(false);
+    setMessageError(false);
+
+    if(!name){
+      setNameError(true);
+    }
+    else if(!email){
+      setEmailError(true);
+    }
+    else if(!title){
+      setTitleError(true);
+    }
+    else if(!message){
+      setMessageError(true);
+    }
+    else{
+      console.log(`name ${name}
+      email: ${email}
+      title: ${title}
+      message: ${message}`)
+    }
+
+  }
+
+  useEffect(()=>{
     
-      <div className={classes.root} id="contact">Hi from Contact</div>
-   
+  })
+  
+  return (
+    <div className={classes.root} id="contact">
+      Contact us:
+      <form noValidate autoComplete="off" className={classes.form} onSubmit={handleSubmit}>
+        <TextField
+        onChange={e => setName(e.target.value)}
+          className={classes.field}
+          label="Name"
+          variant="outlined"
+          fullWidth
+          required
+          error={nameError}
+        />
+        <TextField
+        onChange={e => setEmail(e.target.value)}
+          className={classes.field}
+          label="Email"
+          variant="outlined"
+          fullWidth
+          required
+          error={emailError}
+        />
+        <TextField
+        onChange={e => setTitle(e.target.value)}
+          className={classes.field}
+          label="Title"
+          variant="outlined"
+          fullWidth
+          required
+          error={titleError}
+        />
+        <TextField
+        onChange={e => setMessage(e.target.value)}
+          className={classes.field}
+          label="Message"
+          variant="outlined"
+          multiline={true}
+          rows={5}
+          fullWidth
+          required
+          error={messageError}
+        />
+        <Button type="submit" variant="contained" color="secondary" fullWidth>Submit</Button>
+      </form>
+    </div>
   );
 }
 
