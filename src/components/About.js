32d9e91827c1b40c2afb2223a9c27a6ad0462983
Laith Alwani <laithalwani@gmail.com>
2021-10-19 09:50:55 -0400

    added gird and cards to about page

--- src/components/About.js
+++ src/components/About.js
@@ -1,21 +1,57 @@
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
+import { Card, CardContent, CardMedia, Container, Grid } from "@mui/material";
 
 function About() {
   return (
-    <Box
+    <Container
       id="about"
       sx={{
         minHeight: "100vh",
-        backgroundImage: `url(${
-          "/images/Kitchen-S.jpg"
-        })`,
-        backgroundSize: "cover",
-        backgroundRepeat: "no-repeat",
-        marginBottom: "5rem",
+        height: "100vh",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
       }}
     >
-      <Box
+      <Box >
+
+      <Typography variant="h4" sx={{textAlign:"center", mb:2}}>About Us</Typography>
+      <Grid container spacing={2}>
+        <Grid item xs={12} sm={4}>
+          <Card sx={{width:300, height:425, margin:"0 auto"}}>
+            <CardMedia component="img" src="https://images.unsplash.com/photo-1549388604-817d15aa0110" alt="" height="300" />
+            <CardContent>
+              <Typography variant="h6">Tilte</Typography>
+              <Typography variant="body2">specializing in
+              kitchens and bathrooms remodeling and renovations</Typography>
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid item xs={12} sm={4}>
+          <Card sx={{width:300, height:425, margin:"0 auto"}}>
+            <CardMedia component="img" src="https://images.unsplash.com/photo-1525097487452-6278ff080c31" alt="" height="300" />
+            <CardContent>
+              <Typography variant="h6">Tilte</Typography>
+              <Typography variant="body2">finishing
+              basements from framing to drywall we do it all</Typography>
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid item xs={12} sm={4}>
+          <Card sx={{width:300, height:425, margin:"0 auto"}}>
+            <CardMedia component="img" src="https://images.unsplash.com/photo-1523413651479-597eb2da0ad6" alt="" height="300" />
+            <CardContent>
+              <Typography variant="h6">Tilte</Typography>
+              <Typography variant="body2">We always
+              providing a lengthy warranty on our work. Quality work,
+              punctuality and cleanliness are always our main priority</Typography>
+            </CardContent>
+          </Card>
+        </Grid>
+      </Grid>
+      </Box>
+      {/* <Box
         sx={{
           height: "100vh",
           display: "flex",
@@ -57,8 +93,8 @@ function About() {
             </Typography>
           </Box>
         </Box>
-      </Box>
-    </Box>
+      </Box> */}
+    </Container>
   );
 }
 
