11a8cb09a95b75734e3d64d62a514e248a3553ad
Laith Alwani <laithalwani@gmail.com>
2021-10-22 14:17:20 -0400

    changed the layout of portfolio

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -4,37 +4,103 @@ import Title from "./Title";
 
 export default function Portfolio() {
   return (
-    <Box sx={{backgroundColor:"#444444", padding:"2rem 0"}}>
+    <Box sx={{ backgroundColor: "#444444", padding: "2rem 0" }}>
       <Container>
-        <Title text="GALLERY" icon={<PermMediaIcon fontSize="large" />} color="white" />
-        <Grid container spacing={1}>
-          <Grid item xs={6} md={3}>
-            <img
-              src="/images/20210608_131330-min.avif"
-              alt=""
-              style={{ width: "100%" }}
-            />
+        <Title
+          text="GALLERY"
+          icon={<PermMediaIcon fontSize="large" />}
+          color="white"
+        />
+        <Grid container spacing={5}>
+          <Grid container spacing={1} item xs={12} md={4} >
+            <Grid item xs={6} >
+              <img
+                src="/images/20210608_131330-min.avif"
+                alt=""
+                style={{ width: "100%" }}
+              />
+            </Grid>
+            <Grid item xs={6} >
+              <img
+                src="/images/20210615_143717-min.avif"
+                alt=""
+                style={{ width: "100%" }}
+              />
+            </Grid>
+            <Grid item xs={6} >
+              <img
+                src="/images/20210716_121341-min.avif"
+                alt=""
+                style={{ width: "100%" }}
+              />
+            </Grid>
+            <Grid item xs={6} >
+              <img
+                src="/images/20210716_121248-min.avif"
+                alt=""
+                style={{ width: "100%" }}
+              />
+            </Grid>
           </Grid>
-          <Grid item xs={6} md={3}>
-            <img
-              src="/images/20210615_143717-min.avif"
-              alt=""
-              style={{ width: "100%" }}
-            />
+          <Grid container spacing={1} item xs={12} md={4}  >
+            <Grid item xs={6} >
+              <img
+                src="/images/20210608_131330-min.avif"
+                alt=""
+                style={{ width: "100%" }}
+              />
+            </Grid>
+            <Grid item xs={6} >
+              <img
+                src="/images/20210615_143717-min.avif"
+                alt=""
+                style={{ width: "100%" }}
+              />
+            </Grid>
+            <Grid item xs={6} >
+              <img
+                src="/images/20210716_121341-min.avif"
+                alt=""
+                style={{ width: "100%" }}
+              />
+            </Grid>
+            <Grid item xs={6} >
+              <img
+                src="/images/20210716_121248-min.avif"
+                alt=""
+                style={{ width: "100%" }}
+              />
+            </Grid>
           </Grid>
-          <Grid item xs={6} md={3}>
-            <img
-              src="/images/20210716_121341-min.avif"
-              alt=""
-              style={{ width: "100%" }}
-            />
-          </Grid>
-          <Grid item xs={6} md={3}>
-            <img
-              src="/images/20210716_121248-min.avif"
-              alt=""
-              style={{ width: "100%" }}
-            />
+          <Grid container spacing={1} item xs={12} md={4}  >
+            <Grid item xs={6} >
+              <img
+                src="/images/20210608_131330-min.avif"
+                alt=""
+                style={{ width: "100%" }}
+              />
+            </Grid>
+            <Grid item xs={6} >
+              <img
+                src="/images/20210615_143717-min.avif"
+                alt=""
+                style={{ width: "100%" }}
+              />
+            </Grid>
+            <Grid item xs={6} >
+              <img
+                src="/images/20210716_121341-min.avif"
+                alt=""
+                style={{ width: "100%" }}
+              />
+            </Grid>
+            <Grid item xs={6} >
+              <img
+                src="/images/20210716_121248-min.avif"
+                alt=""
+                style={{ width: "100%" }}
+              />
+            </Grid>
           </Grid>
         </Grid>
       </Container>
