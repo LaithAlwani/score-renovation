a4b1108a83b37939ae0d69fca03a007571f29751
Laith Alwani <laithalwani@gmail.com>
2022-01-30 00:14:54 -0500

    fixed problem with images

--- src/utils/aboutInfo.js
+++ src/utils/aboutInfo.js
@@ -2,19 +2,19 @@ const aboutInfo = [
   {
     title:"Remodel",
     text:"Specializing in kitchens and bathrooms remodeling and renovations.",
-    image:"images/about-kitchen.jpg",
+    image:"/images/about-kitchen.jpg",
     alt:"kitchen renovation"
   },
   {
     title:"Finish",
     text:"From framing to drywalls we renovate any room in your house.",
-    image:"images/about-basement.jpeg",
+    image:"/images/about-basement.jpeg",
     alt:"basement renovation"
   },
   {
     title:"Quality",
     text:"Punctuality and cleanliness are always our main priority in any renovation, backed by our lengthy warranty.",
-    image:"images/about-bath.jpg",
+    image:"/images/about-bath.jpg",
     alt:"kitchen renovation"
   },
 ]
